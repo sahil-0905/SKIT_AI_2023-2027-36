e56ae8f0092c94b42329ef88d29de776aa302b99
Rudra Pratap Singh <pratapsinghr782@gmail.com>
2026-09-23 15:42:52 +0530

    server models added

---
+++ server/models/Problem.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const testCaseSchema = new mongoose.Schema({
+  input: {
+    type: String,
+    required: true,
+  },
+  expectedOutput: {
+    type: String,
+    required: true,
+  },
+  isHidden: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const problemSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    difficulty: {
+      type: String,
+      enum: ["Easy", "Medium", "Hard"],
+      required: true,
+    },
+    topic: {
+      type: String,
+      required: true,
+    },
+    testCases: [testCaseSchema],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Problem", problemSchema);
